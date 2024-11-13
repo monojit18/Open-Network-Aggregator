8862304057468dce7cce6e03adbe0f858c1c19b8
monojit18 <datta.monojit@gmail.com>
2024-11-13 19:51:20 +0530

    refactoring

--- frontend/web/src/app/environment/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  SEEKER_RECEIVER_HTTP_HOST:
-    'https://seeker-receiver-801148443625.asia-southeast1.run.app',
-  BAP_URI: 'https://master-agent-801148443625.asia-southeast1.run.app',
-};
