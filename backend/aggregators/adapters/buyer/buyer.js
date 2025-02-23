50f0ffafcdea87f39e4a8911d979c0e85ebc9642
monojit18 <datta.monojit@gmail.com>
2025-02-23 12:21:05 +0530

    Update buyer.js

--- backend/aggregators/adapters/buyer/buyer.js
+++ backend/aggregators/adapters/buyer/buyer.js
@@ -225,7 +225,7 @@ _express.post("/search", async (request, response) =>
 });
 /* API DEFINITIONS - END */
 
-var port = process.env.port || process.env.PORT || 12001;
+var port = process.env.port || process.env.PORT || 10001;
 _server.listen(port);
 
 initializeBuyer();
