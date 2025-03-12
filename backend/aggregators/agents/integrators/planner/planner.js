fbd04733b62b65a23d968d410795851733c016c3
monojit18 <datta.monojit@gmail.com>
2025-03-13 03:08:48 +0530

    Update planner.js
    Adding support for planner

--- backend/aggregators/agents/integrators/planner/planner.js
+++ backend/aggregators/agents/integrators/planner/planner.js
@@ -41,6 +41,7 @@ const KMicroServices =
 const KDomainNames =
 {
     ONDC: "ONDC",
+    VIDEO: "VIDEO",
     LLM: "LLM"
 }
 
@@ -82,23 +83,24 @@ function processGenericResponse(response)
 
 function prepareAllUrls()
 {
-    // _allUrls[KMicroServices.GenAITextlib] = `${process.env.GENAI_TEXTLIB_HOST}`;
+    _allUrls[KMicroServices.GenAITextlib] = `${process.env.GENAI_TEXTLIB_HOST}`;
     _allUrls[KMicroServices.ONDCAgent] = `${process.env.ONDC_AGENT_URL}`;
     _allUrls[KMicroServices.LLMAgent] = `${process.env.LLM_AGENT_URL}`;
-    // _allUrls[KMicroServices.VideoAgent] = `${process.env.VIDEO_AGENT_URL}`;
+    _allUrls[KMicroServices.VideoAgent] = `${process.env.VIDEO_AGENT_URL}`;
 }
 
-// function prepareExtractionInfo(request)
-// {
-//     const extractionInfo = {};
-//     extractionInfo.context = request.body.context;
-//     extractionInfo.message = request.body.message;  
-//     extractionInfo.preferred_network = request.body.preferred_network;
-//     extractionInfo.preferred_networks = request.body.preferred_networks;    
-//     extractionInfo.prompt = process.env.PLANNER_EXTRACT_NLP_PROMPT;
-//     extractionInfo.endpointId = process.env.AGENTIC_MODEL_ENDPOINT_ID;
-//     return extractionInfo;
-// }
+function prepareMultiSearchInfo(request)
+{
+    const extractionInfo = {};
+    extractionInfo.context = request.body.context;
+    extractionInfo.message = request.body.message;
+    extractionInfo.headers = request.headers;
+    extractionInfo.preferred_network = request.body.preferred_network;
+    extractionInfo.preferred_networks = request.body.preferred_networks;    
+    extractionInfo.prompt = process.env.PLANNER_EXTRACT_NLP_PROMPT;
+    extractionInfo.endpointId = process.env.AGENTIC_MODEL_ENDPOINT_ID;    
+    return extractionInfo;
+}
 
 function prepareAgriInfo(request)
 {
@@ -109,40 +111,40 @@ function prepareAgriInfo(request)
     return transactionInfo;
 }
 
-// function preparePlannerContentInfo(promptInfo)
-// {
-//     const contentInfo = {};
-//     const partInfo = {};
-//     partInfo.text = promptInfo.prompt;
+function prepareExtractContentInfo(promptInfo)
+{
+    const contentInfo = {};
+    const partInfo = {};
+    partInfo.text = promptInfo.prompt;
 
-//     contentInfo.role = "user";
-//     contentInfo.parts = [];
-//     contentInfo.parts.push(partInfo);
-//     return [contentInfo];
-// }
+    contentInfo.role = "user";
+    contentInfo.parts = [];
+    contentInfo.parts.push(partInfo);
+    return [contentInfo];
+}
 
-// function prepareInstructionContentInfo(plannerInfo)
-// {
-//     const instruction = {};
+function prepareInstructionContentInfo(plannerInfo)
+{
+    const instruction = {};
 
-//     const partInfo = {};
-//     partInfo.text = plannerInfo.prompt;
+    const partInfo = {};
+    partInfo.text = plannerInfo.prompt;
 
-//     instruction.parts = [];
-//     instruction.parts.push(partInfo);
-//     return instruction;
-// }
+    instruction.parts = [];
+    instruction.parts.push(partInfo);
+    return instruction;
+}
 
-// function prepareShortHeaders(endpointId)
-// {
-//     const genAIHeaders = {};
-//     genAIHeaders.temperature = 0.4;
-//     genAIHeaders.maxtokens = 2048;
-//     genAIHeaders.topk = 40;
-//     genAIHeaders.topp = 0.95;
-//     genAIHeaders.endpointid = endpointId;
-//     return genAIHeaders;
-// }
+function prepareShortHeaders(endpointId)
+{
+    const genAIHeaders = {};
+    genAIHeaders.temperature = 0.4;
+    genAIHeaders.maxtokens = 2048;
+    genAIHeaders.topk = 40;
+    genAIHeaders.topp = 0.95;
+    genAIHeaders.endpointid = endpointId;
+    return genAIHeaders;
+}
 
 function prepareAckResponse(plannerInfo)
 {
@@ -169,17 +171,13 @@ function prepareAgentRequest(plannerInfo, domainName)
     return requestBody;
 }
 
-// function prepareVideoAgentRequest(planner, plannerInfo)
-// {
-//     const requestBody = {};
-//     requestBody.context = plannerInfo.context;
-//     requestBody.context.domain = `${process.env.AGENTIC_DOMAIN_PREFIX}${planner.domain}`;
-//     requestBody.message = plannerInfo.message;
-//     requestBody.preferred_network = plannerInfo.preferred_networks[planner.domain];
-//     requestBody.preferred_networks = plannerInfo.preferred_networks;
-//     requestBody.message.network.filters.query = planner.relevant_text;
-//     return requestBody;
-// }
+function prepareVideoAgentRequest(plannerInfo)
+{
+    const requestBody = prepareAgentRequest(plannerInfo, KDomainNames.VIDEO);
+    requestBody.preferred_network = plannerInfo.preferred_networks[KDomainNames.VIDEO];    
+    requestBody.message.network.filters.query = plannerInfo.message.network.filters.query;
+    return requestBody;
+}
 
 function prepareONDCAgentRequest(plannerInfo)
 {
@@ -192,23 +190,29 @@ function prepareONDCAgentRequest(plannerInfo)
 function prepareLLMAgentRequest(plannerInfo)
 {
     const requestBody = prepareAgentRequest(plannerInfo, KDomainNames.LLM);
-    requestBody.message.network.filters[0].type = KConstantValues.ADVISORY;
+
+    const filters = requestBody.message.network.filters;
+    if (Array.isArray(filters) == true)
+        filters[0].type = KConstantValues.ADVISORY;
+    else
+        filters.type = KConstantValues.ADVISORY;
+
     requestBody.message.network.relevant_text = KConstantValues.EMPTY;
     return requestBody;
 }
 
-// function prepareExtractRequest(plannerInfo)
-// {
-//     const requestBody = {};
+function prepareExtractRequest(plannerInfo)
+{
+    const requestBody = {};
 
-//     const promptInfo = {};
-//     promptInfo.prompt = plannerInfo.message?.network?.relevant_text;
-//     const contentsList = preparePlannerContentInfo(promptInfo);
+    const promptInfo = {};
+    promptInfo.prompt = plannerInfo.message?.network?.filters.query;
+    const contentsList = prepareExtractContentInfo(promptInfo);
 
-//     requestBody.contents = contentsList;
-//     requestBody.instruction = prepareInstructionContentInfo(plannerInfo);
-//     return requestBody;
-// }
+    requestBody.contents = contentsList;
+    requestBody.instruction = prepareInstructionContentInfo(plannerInfo);
+    return requestBody;
+}
 
 function initializePlanner()
 {
@@ -220,34 +224,34 @@ function initializePlanner()
     prepareAllUrls();
 }
 
-// async function callVideoAgent(planner, extractionInfo)
-// {
-//     const requestOptions = {};
-//     requestOptions.httpsAgent = _axiosAgent;
-//     requestOptions.headers = {};
-//     requestOptions.headers[process.env.VIDEO_API_KEY] = extractionInfo.headers[process.env.VIDEO_API_KEY];
+async function callVideoAgent(extractionInfo)
+{
+    const requestOptions = {};
+    requestOptions.httpsAgent = _axiosAgent;
+    requestOptions.headers = {};
+    requestOptions.headers[process.env.VIDEO_API_KEY] = extractionInfo.headers[process.env.VIDEO_API_KEY];
 
-//     const requestBody = prepareVideoAgentRequest(planner, extractionInfo);
+    const requestBody = prepareVideoAgentRequest(extractionInfo);
     
-//     try
-//     {
-//         const agentResponse = await Axios.post(`${_allUrls[KMicroServices.VideoAgent]}/search`,
-//                                                     requestBody, requestOptions);
-//         const agentResult = processGenericResponse(agentResponse);
-//         return agentResult;        
-//     }
-//     catch(exception)
-//     {
-//         throw exception;
-//     }
-// }
+    try
+    {
+        const agentResponse = await Axios.post(`${_allUrls[KMicroServices.VideoAgent]}/search`,
+                                                    requestBody, requestOptions);
+        const agentResult = processGenericResponse(agentResponse);
+        return agentResult;        
+    }
+    catch(exception)
+    {
+        throw exception;
+    }
+}
 
-async function callONDCAgent(agriInfo)
+async function callONDCAgent(ondcInfo)
 {
     const requestOptions = {};
     requestOptions.httpsAgent = _axiosAgent;
 
-    const requestBody = prepareONDCAgentRequest(agriInfo);
+    const requestBody = prepareONDCAgentRequest(ondcInfo);
     
     try
     {
@@ -262,16 +266,16 @@ async function callONDCAgent(agriInfo)
     }
 }
 
-async function callLLMAgent(agriInfo)
+async function callLLMAgent(llmInfo)
 {
     const requestOptions = {};
     requestOptions.httpsAgent = _axiosAgent;
 
-    const requestBody = prepareLLMAgentRequest(agriInfo);
+    const requestBody = prepareLLMAgentRequest(llmInfo);
     
     try
     {
-        const agentResponse = await Axios.post(`${_allUrls[KMicroServices.LLMAgent]}/search`,
+        const agentResponse = await Axios.post(`${_allUrls[KMicroServices.LLMAgent]}/search/llm`,
                                                     requestBody, requestOptions);
         const agentResult = processGenericResponse(agentResponse);
         return agentResult;
@@ -282,76 +286,86 @@ async function callLLMAgent(agriInfo)
     }
 }
 
-// async function performPlannerSearch(plannerList, extractionInfo)
-// {    
-//     await Promise.all(plannerList.map(async (planner) =>
-//     {
-//         switch(planner.domain)
-//         {
-//             case "ONDC":
-//             {
-//                 // await callONDCAgent(planner, extractionInfo);
-//             }
-//             break;
+async function performExtractedSearch(extractedList, extractionInfo)
+{
+    try
+    {
+        await Promise.all(extractedList.map(async (item) =>
+        {
+            switch(item.domain)
+            {
+                case KDomainNames.ONDC:
+                {
+                    extractionInfo.message.network.relevant_text = item.relevant_text;
+                    await callONDCAgent(extractionInfo);
+                }
+                break;
+    
+                case KDomainNames.VIDEO:
+                {
+                    extractionInfo.message.network.filters.query = item.relevant_text;
+                    await callVideoAgent(extractionInfo);
+                }
+                break;
+    
+                case KDomainNames.LLM:
+                {
+                    extractionInfo.message.network.relevant_text = item.relevant_text;
+                    await callLLMAgent(extractionInfo);
+                }
+                break;
+            }
+        }));
+    }
+    catch(exception)
+    {
+        throw exception;
+    }
+}
 
-//             case "VIDEO":
-//             {
-//                 // await callVideoAgent(planner, extractionInfo);
-//             }
-//             break;
+async function performMultiSearch(extractionInfo)
+{
+    const requestOptions = {};
+    requestOptions.httpsAgent = _axiosAgent;
 
-//             case "LLM":
-//             {
-//                 // await callLLMAdapter(extractionInfo);
-//             }
-//             break;
-//         }
-//     }));
-// }
+    const requestBody = prepareExtractRequest(extractionInfo);    
+    const genAIHeaders = prepareShortHeaders(extractionInfo.endpointId);
+    requestOptions.headers = genAIHeaders;
 
-// async function performPlannerExtract(extractionInfo)
-// {
-//     const requestOptions = {};
-//     requestOptions.httpsAgent = _axiosAgent;
-
-//     const requestBody = prepareExtractRequest(extractionInfo);    
-//     const genAIHeaders = prepareShortHeaders(extractionInfo.endpointId);
-//     requestOptions.headers = genAIHeaders;
-
-//     try
-//     {
-//         let response = await Axios.post(`${_allUrls[KMicroServices.GenAITextlib]}/genai/text?type=json`,
-//                                                 requestBody, requestOptions);
-//         const plannerResponse = processGenericResponse(response);
-//         const plannerList = plannerResponse.results[0].formatted_response;
-//         await performPlannerSearch(plannerList, extractionInfo);
-//     }
-//     catch(exception)
-//     {
-//         throw exception;
-//     }
-// }
+    try
+    {
+        let response = await Axios.post(`${_allUrls[KMicroServices.GenAITextlib]}/genai/text?type=json`,
+                                                requestBody, requestOptions);
+        const extractedResponse = processGenericResponse(response);
+        const extractedList = extractedResponse.results[0].formatted_response;
+        await performExtractedSearch(extractedList, extractionInfo);
+    }
+    catch(exception)
+    {
+        throw exception;
+    }
+}
 
 /* API DEFINITIONS - START */
-// _express.post("/extract/search", async (request, response) =>
-// {
-//     const extractionInfo = prepareExtractionInfo(request);
-//     const results = {};
+_express.post("/multi/search", async (request, response) =>
+{
+    const extractionInfo = prepareMultiSearchInfo(request);
+    const results = {};
 
-//     try
-//     {
-//         const ackResponse = prepareAckResponse(extractionInfo);
-//         results.results = ackResponse;
-//         response.send(results);
-//         await performPlannerExtract(extractionInfo);
-//     }
-//     catch(exception)
-//     {
-//         let errorInfo = prepareErrorMessage(exception);
-//         results.results = errorInfo.message;
-//         response.status(errorInfo.code).send(results);;
-//     }
-// });
+    try
+    {
+        const ackResponse = prepareAckResponse(extractionInfo);
+        results.results = ackResponse;
+        response.send(results);
+        await performMultiSearch(extractionInfo);
+    }
+    catch(exception)
+    {
+        let errorInfo = prepareErrorMessage(exception);
+        results.results = errorInfo.message;
+        response.status(errorInfo.code).send(results);;
+    }
+});
 
 _express.post("/agri/search", async (request, response) =>
 {
